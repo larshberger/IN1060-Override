dde938deef419d7fecc4022cfb598779e5fbefe4
Lars Husby Berger <144447964+larshberger@users.noreply.github.com>
2024-05-23 14:10:49 +0200

    Update fullstendigLoesningv1.ino
    Slettet alle kommentarer

--- fullstendigLoesningv1.ino
+++ fullstendigLoesningv1.ino
@@ -1,6 +1,4 @@
 #include <FastLED.h>
-
-//Definering og initialisering av diverse makroer og variabler
 #define farge   CRGB::Purple
 #define led_port  8
 #define ant_leds  15
@@ -23,10 +21,8 @@ void setup() {
 void loop() {
   int lysVerdi = analogRead(sensor);
 
-  //Vår på/av. Dersom fototransistoren leser en verdi under en forutsatt terskel starter lysene
   if(lysVerdi < terskel){
 
-    //Vi har litt ekstra kode for når lysene starter opp
     if (indeks == 0){       
       startTid = millis();
       forrigeOppdateringTid = millis();
@@ -36,7 +32,6 @@ void loop() {
       indeks++;
     }
     
-    //Hvis riktig tid har gått siden forrige lys ble aktivert, skrur vi på neste lys
     unsigned long p = forrigeOppdateringTid + forsinkelse;
     if(millis() > p){        
       skruPaaLys(indeks);
@@ -45,27 +40,22 @@ void loop() {
       forrigeOppdateringTid = millis();
     }
 
-    //Hvis riktig tid har gått siden flasken ble satt på blinker vi rødt.
     else if(millis() > startTid + animasjonsTid){     
       blinkRoedt();
     }
   }
 
-  //Dersom fototransistoren leser for høy verdi skrus lysene av og indeksen nullstilles.
   else{
     lysAv();
     indeks = 0;
   }
 }
 
-//Denne metoden skrur på en led hvems indeks vi tar som parameter i metodekallet
 void skruPaaLys(int indeks){
   leds[indeks] = farge;
   FastLED.show();
 }
 
-//Denne metoden skrur av alle lysene UNNTATT det første og siste lyset. 
-//Disse skal lyse hele tiden for at fototransistoren skal kunne registrere en verdi selv i mørke rom
 void lysAv(){
   FastLED.clear();
   leds[0] = CRGB::White;
@@ -73,7 +63,6 @@ void lysAv(){
   FastLED.show();
 }
 
-//Denne metoden blinker alle lysene rødt. 
 void blinkRoedt(){
   for (int n = 0; n < ant_leds; n++) {leds[n] = CRGB::Red;}
   FastLED.show();
